cacc71f75c944abde7088f7a91f8f8dd717f5d41
iblind <no@email.com>
2020-03-26 18:57:17 -0400

    transitions

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -10,16 +10,63 @@ let $map;
 function resize() {}
 
 function updateMapBack(el) {
-  console.log(el)
+  const currentStep = el.getAttribute('data-previous-step')
+  if (currentStep === 'slide1') {
+    console.log(currentStep)
+
+    console.log('CHANGING COLOR BLACK')
+    $map.setPaintProperty('local-vs-tourist-circles', 'circle-color', '#000000');
+
+  } else if (currentStep === 'slide2') {
+    console.log(currentStep)
+
+    console.log('CHANGING COLOR WHITE')
+    $map.setPaintProperty('local-vs-tourist-circles', 'circle-color', '#ffffff');
+
+  }
 }
 
 function updateMap(el) {
-  console.log(el)
+  const currentStep = el.getAttribute('data-step')
+
+  if (currentStep === 'slide1') {
+    console.log(currentStep)
+
+    console.log('CHANGING COLOR BLACK')
+    $map.setPaintProperty('local-vs-tourist-circles', 'circle-color', '#000000');
+
+  } else if (currentStep === 'slide2') {
+    console.log(currentStep)
+
+    $map.setPaintProperty('local-vs-tourist-circles', 'circle-radius',
+      [
+        "interpolate",
+        ["cubic-bezier", 0.1, 1, 0.2, 1],
+        ["get", "total"], 0, 3, 1, 2, 85936, 20
+      ])
+
+    $map.setPaintProperty('local-vs-tourist-circles', 'circle-stroke-color', 'hsl(0, 0%, 100%)')
+    $map.setPaintProperty('local-vs-tourist-circles', 'circle-stroke-width', 0.2)
+
+    "hsl(0, 0%, 100%)"
+
+  } else if (currentStep === 'slide3') {
+
+    $map.setPaintProperty('local-vs-tourist-circles', 'circle-color', [
+      "interpolate",
+      ["linear"],
+      ["get", "pct_local"], 0, "#bf3a63", 1, "#3c58eb"
+    ])
+  } else if (currentStep === 'slide4') {
+
+  }
+
 }
 
 
 function setupEnterView() {
 
+  console.log('enter view setting up')
   enterView({
     selector: '.story-step',
     enter(el) {
@@ -31,7 +78,7 @@ function setupEnterView() {
     progress(el, progress) {
       //   el.style.opacity = progress;
     },
-    offset: 0.4, // enter at middle of viewport
+    offset: 0.05, // enter at middle of viewport
     once: false, // trigger just once
   });
 }
@@ -68,6 +115,7 @@ function makeMap() {
 
   $map.on('mousemove', e => {
 
+    // console.log($map.queryRenderedFeatures(e))
 
     const pointFeatures = $map.queryRenderedFeatures(e.point)
     const relevantLayer = "local_vs_tourist-0aaeny"
@@ -80,12 +128,17 @@ function makeMap() {
     // console.log(e.lngLat.wrap());
   })
 
+  return $map
+
 }
 
 function init() {
   setupDOM()
-  makeMap()
-  setupEnterView()
+  $map = makeMap()
+  $map.on('load', () => {
+    setupEnterView()
+  })
+
 }
 
 
